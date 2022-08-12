0b439521eb34c193bcc89f3eb04989ed81727117
Ghis8 <ghislainkongolo@yahoo.com>
2022-08-12 12:34:32 +0200

    dynamic age

--- js/script.js
+++ js/script.js
@@ -7,6 +7,16 @@ var typed=new Typed('.typing',{
 	loop:true
 })
 
+//Get Age
+
+let date=new Date()
+let bYear=1998
+let age=date.getFullYear()-bYear
+if(date.getDate()=== 15 && date.getMonth()=== 9){
+	document.getElementById('age').innerHTML=age
+}else{
+	document.getElementById('age').innerHTML=age-1
+}
 
 
 
